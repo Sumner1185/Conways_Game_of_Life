782cd91b8d3a76454d055e57e03ecc5e8ea39c2b
Sumner1185 <alastair.sumner@yahoo.com>
2020-08-05 11:04:59 +0100

    Test for next state to equal dead for cell with one neighbour

--- Cell.js
+++ Cell.js
@@ -2,4 +2,8 @@ export default class Cell {
   constructor(state) {
     this.state = state;
   }
+
+  getNextState(numNeighbours) {
+    return 0
+  }
 }
--- test/Cell.test.js
+++ test/Cell.test.js
@@ -6,8 +6,14 @@ describe('Cell', () => {
   it('Should be initialised with a cellState', () => {
     const aliveCell = new Cell(CellState.ALIVE);
     expect(aliveCell.state).to.equal(CellState.ALIVE);
-    
+
     const deadCell = new Cell(CellState.DEAD);
     expect(deadCell.state).to.equal(CellState.DEAD);
+  })
+
+  it('Should die if it has fewer than 2 live neighbours', () => {
+    const cell = new Cell(CellState.ALIVE);
+    const nextState = cell.getNextState(1);
+    expect(nextState).to.equal(CellState.DEAD)
   })
 })
