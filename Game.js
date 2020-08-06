e2de83abab0ba1cee9d848eb885728b49833ee70
Sumner1185 <alastair.sumner@yahoo.com>
2020-08-06 09:56:55 +0100

    First refactor getNumOfAliveNeighbours function

--- Game.js
+++ Game.js
@@ -13,39 +13,30 @@ export default class Game {
   }
 
   getNumOfAliveNeighbours(row, col) {
-    const edgeCell = new Cell(CellState.DEAD);
-
-    const rowAbove = this.state[row - 1];
-
-    const topLeft = (row === 0 || col === 0) ? edgeCell : rowAbove[col - 1];
-    const top = row === 0 ? edgeCell : rowAbove[col];
-    const topRight = (row === 0 || col === this.numCols -1) ? edgeCell : rowAbove[col + 1];
-
-    const rowBelow = this.state[row + 1];
-
-    const bottomLeft = (col === 0 || row === this.numRows - 1) ? edgeCell : rowBelow[col - 1];
-    const bottom = row === this.numRows - 1 ? edgeCell : rowBelow[col];
-    const bottomRight = (row === this.numRows -1 || col === this.numCols - 1) ? edgeCell : rowBelow[col + 1];
-
-    const thisRow = this.state[row];
-
-    const left = col === 0 ? edgeCell : thisRow[col - 1];
-    const right = col === this.numCols -1 ? edgeCell : thisRow[col + 1];
-
     const stateValues = {
       [CellState.ALIVE]: 1,
       [CellState.DEAD]: 0,
     }
 
-    return [
-      topLeft,
-      top,
-      topRight,
-      left,
-      right,
-      bottomLeft,
-      bottom,
-      bottomRight
-    ].reduce((sum, { state }) => sum + stateValues[state], 0);
+    let numNeighbours = 0;
+
+    this.state.forEach((cellRow, rowNum) => {
+      cellRow.forEach((cell, colNum) => {
+      if ((colNum === col - 1 && rowNum === row - 1)
+        || (colNum === col && rowNum === row - 1)
+        || (colNum === col + 1 && rowNum === row - 1)) {
+          numNeighbours += stateValues[cell.state];
+        } else if ((colNum === col - 1 && rowNum === row)
+        || (colNum === col + 1 && rowNum === row)) {
+          numNeighbours += stateValues[cell.state];
+        } else if ((colNum === col - 1 && rowNum === row  + 1)
+        || (colNum === col && rowNum === row + 1)
+        || (colNum === col + 1 && rowNum === row + 1)) {
+          numNeighbours += stateValues[cell.state];
+        }
+      });
+    });
+
+    return numNeighbours;
   }
 }
